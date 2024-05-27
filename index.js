7ed6d029be7d8d84e046df0caec7f0629cb7185a
KatkamRavali <ravalikatkam681@gmail.com>
2024-05-28 00:23:51 +0530

    Completed the JS Project

--- index.js
+++ index.js
@@ -37,7 +37,7 @@
 // State : It is used to store anything [but here state is just a variable]
 // ------------------------- Used to store on the array list [backup storage] -----------------------------
 const state = {
-  taskList : [],
+    taskList : [],
 };
 
 // DOM Operations syntax : dom.methods
@@ -51,74 +51,74 @@ const taskModal = document.querySelector(".task__modal_body"); // task__modal_bo
 // ----------------------- Template for the Card on the screen --------------------------------
 // here in 18th line inside class : task__card is the define name 
 const htmlTaskContents = ({id , url , title , type , description}) =>  ` 
-  <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
-      <div class="card shadow-sm task__card"> 
+    <div class="col-md-6 col-lg-4 mt-3 mb-10" id=${id} key=${id}>
+        <div class="card shadow-sm task__card"> 
           
-          <div class="card-header d-flex justify-content-end task__card_header">
-              <button type="button" class="btn btn-outline-info me-2" name=${id} onclick="EditTask.apply()">
-                  <i class="fa-solid fa-pen-to-square name=${id}"></i>
-              </button>
-              <button type="button" class="btn btn-outline-danger" name=${id} onclick="DeleteTask.apply()">
-                  <i class="fa-solid fa-trash name=${id}"></i>
-              </button>
-          </div>
+            <div class="card-header d-flex justify-content-end task__card_header">
+                <button type="button" class="btn btn-outline-info me-2" name=${id} onclick="EditTask.apply()">
+                    <i class="fa-solid fa-pen-to-square name=${id}"></i>
+                </button>
+                <button type="button" class="btn btn-outline-danger" name=${id} onclick="DeleteTask.apply()">
+                    <i class="fa-solid fa-trash name=${id}"></i>
+                </button>
+            </div>
 
-          <div class='card-body'>
-              ${
-                // --------------- This is for [if we have image it will display or else no ] ----------
-                //  url &&
-                //  `<img src=${url} class="card-img-top md-3 rounded-lg" alt="Card Image" />`
+            <div class='card-body'>
+                ${
+                  // --------------- This is for [if we have image it will display or else no ] ----------
+                  //  url &&
+                  //  `<img src=${url} class="card-img-top md-3 rounded-lg" alt="Card Image" />`
 
-                // --------------- This is for [if we have image it will display or else no it will display default image ] ----------
-                url ?
-                `<img src=${url} class="img-fluid place__holder__image mb-3" alt="Card Image">`
-                : `<img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/DefaultImage.png" 
-                        class="img-fluid place__holder__image mb-3" 
-                        alt="Card Image">`
-              }
-              <h4 class="card-title task__card_title">${title}</h4>
-              <p class="description trim-3-lines text-muted">${description}</p>
-              <div class="taskType text-white d-flex flex-wrap">
-                  <span class="badge text-bg-info m-1">${type}</span>
-              </div>
-          </div>
+                  // --------------- This is for [if we have image it will display or else no it will display default image ] ----------
+                  url ?
+                  `<img src=${url} class="img-fluid place__holder__image mb-3" alt="Card Image">`
+                  : `<img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/DefaultImage.png" 
+                          class="img-fluid place__holder__image mb-3" 
+                          alt="Card Image">`
+                }
+                <h4 class="card-title task__card_title">${title}</h4>
+                <p class="description trim-3-lines text-muted">${description}</p>
+                <div class="taskType text-white d-flex flex-wrap">
+                    <span class="badge text-bg-info m-1">${type}</span>
+                </div>
+            </div>
 
-          <div class="card-footer">
+            <div class="card-footer">
                 <button type="button" 
-                      class="btn btn-outline-info float-right" 
-                      data-bs-toggle="modal" 
-                      data-bs-target="#OpenTask"
-                      onclick="ToOpenTask()"
-                      id=${id}>Open Task
+                    class="btn btn-outline-info float-right" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#OpenTask"
+                    onclick="ToOpenTask()"
+                    id=${id}>Open Task
                 </button> 
-          </div>
-      </div>
-  </div>
+            </div>
+        </div>
+    </div>
 `;
 
 // ------------------------ Template for Card when clicked on Open Task -------------------------------
 const htmlTaskBody = ({id , url , title , description}) =>  {
-  const date = new Date(parseInt(id));
-  return `
-      <div id=${id}>
-          ${
-            // --------------- This is for [if we have image it will display or else no ] ----------
-            //   url &&
-            //   `<img src=${url} class="img-fluid place__holder__image mb-3" alt="Card Image">`
+    const date = new Date(parseInt(id));
+    return `
+        <div id=${id} class="card-Details">
+            ${
+              // --------------- This is for [if we have image it will display or else no ] ----------
+              //   url &&
+              //   `<img src=${url} class="img-fluid place__holder__image mb-3" alt="Card Image">`
 
-            // --------------- This is for [if we have image it will display or else no it will display default image ] ----------
-            url ?
-            `<img width="100%" src=${url} class="img-fluid place__holder__image mb-3" alt="Card Image">`
-            : `<img width="100%" src="https://upload.wikimedia.org/wikipedia/commons/0/0e/DefaultImage.png"
-                    class="img-fluid place__holder__image mb-3" 
-                    alt="Card Image">`
-          }
-          <strong class="text-muted text-sm">Created on : ${date.toDateString()}</strong>
-          <h2 class="mb-3">${title}</h2>
-          <p class="text-muted">${description}</p>
-      </div>
-  `;
-}
+              // --------------- This is for [if we have image it will display or else no it will display default image ] ----------
+              url ?
+              `<img width="100%" src=${url} class="img-fluid place__holder__image mb-1" alt="Card Image">`
+              : `<img width="100%" src="https://upload.wikimedia.org/wikipedia/commons/0/0e/DefaultImage.png"
+                      class="img-fluid place__holder__image mb-3" 
+                      alt="Card Image">`
+            }
+            <strong class="text-sm mb-2">Created on : ${date.toDateString()}</strong>
+            <h2 class="mb-2">${title}</h2>
+            <p class="mb-4">${description}</p>
+        </div>
+    `;
+};
 
 // ------------------- Used to update / store data to the local storage ------------------------------
 //  Why we need to store on the local storage
@@ -126,48 +126,49 @@ const htmlTaskBody = ({id , url , title , description}) =>  {
 // to the browser ]
 // Where we convert JSON > str (i.e.., for local storage)
 const updateLocalStorage = () => {
-  localStorage.setItem(
-      "task", // Key and name should always be in a string formate . Here name is the task
-      JSON.stringify({
-          tasks : state.taskList, // Here key is the least of elements present in an array taskList
-      })
-  );
+    localStorage.setItem(
+        "task", // Key and name should always be in a string formate . Here name is the task
+        JSON.stringify({
+            tasks : state.taskList, // Here key is the least of elements present in an array taskList
+        })
+    );
 };
 
 //  Load initial data
 // Where we convert JSON < str (i.e.., for getting the cards on the screen back)
 const loadInitialData = () => {
-  const localStorageCopy = JSON.parse(localStorage.task);
+    const localStorageCopy = JSON.parse(localStorage.task);
 
-  if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+    if(localStorageCopy) state.taskList = localStorageCopy.tasks;
 
-  // Mapping
-  state.taskList.map((cardDate) => {
-      taskContents.insertAdjacentHTML("beforeend",htmlTaskContents(cardDate));
-  });
-}
+    // Mapping
+    state.taskList.map((cardDate) => {
+        taskContents.insertAdjacentHTML("beforeend",htmlTaskContents(cardDate));
+    });
+};
 
 // When we update or when we edit ... we need to save [this is to get the field items from html to js file ]
 const handleSubmit = (event) => {
-  console.log("event Triggered");
-  // Here date.now changes the value each time you reload so id is unique u gave the attribute as Date.now() 
-  const id = `${Date.now()}`;
-  const input = {
-      url : document.getElementById("ImageUrl").value,  // imageUrl is the id name of the Image url
-      title : document.getElementById("taskTitle").value,
-      type : document.getElementById("taskType").value,
-      description : document.getElementById("taskDescription").value,
-  };
+    console.log("event Triggered");
+    // Here date.now changes the value each time you reload so id is unique u gave the attribute as Date.now() 
+    const id = `${Date.now()}`;
+    const input = {
+        url : document.getElementById("ImageUrl").value,  // imageUrl is the id name of the Image url
+        title : document.getElementById("taskTitle").value,
+        type : document.getElementById("taskType").value,
+        description : document.getElementById("taskDescription").value,
+    };
 
-  if (input.title === "" || input.type  === "" || input.description === "" ) {
-      return alert("Please fill the necessary details");
-  }
-      // To edit the content and display it on the screen [this is to get the updated data from js file to html] 
-      taskContents.insertAdjacentHTML("beforeend",htmlTaskContents({ ...input , id }));
-      // To store the updated data in the array called taskList in variable state
-      state.taskList.push({ ...input , id });
-      //  To store the things on the browser
-      updateLocalStorage();
+    if (input.title === "" || input.type  === "" || input.description === "" ) {
+        return alert("Please fill the necessary details");
+    }
+
+    // To edit the content and display it on the screen [this is to get the updated data from js file to html] 
+    taskContents.insertAdjacentHTML("beforeend",htmlTaskContents({ ...input , id }));
+    // To store the updated data in the array called taskList in variable state
+    state.taskList.push({ ...input , id });
+    //  To store the things on the browser
+    updateLocalStorage();
 };
 
 // Open Task [ When we click on open task we need the same details in a big screen to be displayed ]
@@ -215,7 +216,6 @@ const EditTask = (e) => {
     const editTypes = e.target.tagName; // To get the tag Name 
 
     let parentNode;
-    let editImage;
     let taskTitle;
     let taskDescription;
     let taskType;
@@ -223,29 +223,103 @@ const EditTask = (e) => {
 
     if(editTypes == "BUTTON") {
         parentNode = e.target.parentNode.parentNode; 
-                   // button . div => class => card-header . div => class => card
+                    // button . div => class => card-header . div => class => card
     } else if (editTypes == "I") {
         parentNode = e.target.parentNode.parentNode.parentNode;
                       // i   . button   . div => class => card-header . div => class => card 
     }
-    taskTitle = parentNode.childNodes[3].childNodes[7].childNodes;
-    console.log(taskTitle);
-
-    editImage = parentNode.childNodes[3].childNodes[1];
+    // taskTitle = parentNode.childNodes[3].childNodes[7].childNodes;
+    // console.log(taskTitle);
 
     // h4[1] . div => class => card-body[2] . div => class => card[3]
     taskTitle = parentNode.childNodes[3].childNodes[3]; 
+    // console.log(taskTitle);
 
     // p[1] . h4[2] . div => class => card-body[3] . div => class => card-header[4] . div => class => card[5]
     taskDescription = parentNode.childNodes[3].childNodes[5];
+    // console.log(taskDescription);
 
     // span[1] . div => class => taskType[2] . p[3] . h4[4] . div => class => card-body[5] . 
     // div => class => card-header[6] . div => class => card[7]
     taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    // console.log(taskType);
 
     // button[1] . div => class => card-footer[2] . div => class => card-body[3] . div => class => card-header[4] .
     //  div => class => card[5]
     SubmitButton = parentNode.childNodes[5].childNodes[1];
+    // console.log(SubmitButton);
 
-    console.log(editImage , taskTitle , taskDescription , taskType , SubmitButton);
-}
+    // console.log(taskTitle , taskDescription , taskType , SubmitButton);
+
+    // Set Attribute helps to edit the things from Js file directly to html file 
+    // Syntax : setAttribute("contenteditable","true or false");
+    taskTitle.setAttribute("contenteditable","true");
+    taskDescription.setAttribute("contenteditable","true");
+    taskType.setAttribute("contenteditable","true");
+
+    SubmitButton.setAttribute("onclick","saveEdit.apply(this , arguments)");
+    // data-bs-toggle="modal" data-bs-target="#OpenTask"
+    SubmitButton.removeAttribute("data-bs-toggle");
+    SubmitButton.removeAttribute("data-bs-target");
+    SubmitButton.innerHTML = "Save Changes";
+};
+
+// Save Edit
+const saveEdit = (e) => {
+    if(!e) e = window.event;
+   
+    const saveId = e.target.id; 
+    const parentNode = e.target.parentNode.parentNode;
+    // button . div => class => card-footer . div => class => card 
+    // console.log(parentNode.childNodes);
+
+    const taskTitle = parentNode.childNodes[3].childNodes[3]; 
+    const taskDescription = parentNode.childNodes[3].childNodes[5];
+    const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    const SubmitButton = parentNode.childNodes[5].childNodes[1];
+
+    const updateData = {
+        Title : taskTitle.innerHTML,
+        Description : taskDescription.innerHTML,
+        Type : taskType.innerHTML,
+    };
+    let stateCopy  = state.taskList;
+
+    stateCopy = stateCopy.map((task) => task.id === saveId
+            ?{
+                  id : task.id,
+                  title : updateData.Title,
+                  description : updateData.Description,
+                  type : updateData.Type,
+                  url : task.url,
+            }
+            : task
+        );
+        state.taskList = stateCopy;
+        updateLocalStorage();
+
+        taskTitle.setAttribute("contenteditable","false");
+        taskDescription.setAttribute("contenteditable","false");
+        taskType.setAttribute("contenteditable","false");
+
+        SubmitButton.setAttribute("onclick","ToOpenTask.apply(this , arguments)");
+        SubmitButton.setAttribute("data-bs-toggle" , "modal");
+        SubmitButton.setAttribute("data-bs-target" , "#OpenTask");
+        SubmitButton.innerHTML = "Open Task";
+};
+
+// Search 
+const searchTask = (e) => {
+    if(!e) e = window.event;
+
+    while(taskContents.firstChild) {
+        taskContents.removeChild(taskContents.firstChild);
+    }
+    const resultData = state.taskList.filter(({title}) => 
+        title.toLowerCase().includes(e.target.value.toLowerCase())
+    ); 
+    // console.log(resultData);
+    resultData.map((cardData) => {
+        taskContents.insertAdjacentHTML("beforeend",htmlTaskBody(cardData))
+    });
+};
